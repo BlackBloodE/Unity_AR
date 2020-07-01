33595bb81edca772b16b21b1ad08d6396bf7fcdf
BlackBloodE <elton9731@gmail.com>
2020-07-02 02:35:20 +0800

    Initial commit

---
+++ AR/Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("速度"), Range(0, 10)]
+    public float speed = 1.5f;
+
+    [Header("停止距離"), Range(0, 5)]
+    public float stopDistance = 2;
+
+    [Header("追蹤目標名稱")]
+    public string targetName = "";
+
+    [Header("爆炸效果")]
+    public GameObject explosion;
+
+    [Header("傷害"), Range(10, 100)]
+    public float damage;
+
+    /// <summary>
+    /// 目標 : 
+    /// </summary>
+    private Transform target;
+
+    /// <summary>
+    /// 導覽網路代理器
+    /// </summary>
+    private NavMeshAgent nma;
+
+    private void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+        nma.speed = speed;
+        nma.stoppingDistance = stopDistance;
+
+        target = GameObject.Find(targetName).transform;
+        nma.SetDestination(target.position);
+    }
+
+    private void CreateEffect()
+    {
+        
+    }
+}
---
+++ AR/Assets/Scripts/RandomAni.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAni : MonoBehaviour
+{
+    private Animator anim;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void Randomani()
+    {
+        int ra = Random.Range(0, 4);
+        ra = ra + 1;
+        string str = ra.ToString();
+
+        anim.SetTrigger(str);
+    }
+}
